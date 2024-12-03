497a4b5ba6c9c73e5442fd93f4d682ef5a0455ab
tomikases5 <chlapik.tomas@gmail.com>
2024-12-03 20:02:56 +0100

    Okrasné úpravy

--- src/main.c
+++ src/main.c
@@ -2,43 +2,17 @@
 #include <stm8s.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "max7219.h"
 
+/* Makra na Porty a Piny*/
+#define CLK_GPIO GPIOB     
+#define CLK_PIN GPIO_PIN_0  
+#define DATA_GPIO GPIOB     
+#define DATA_PIN GPIO_PIN_2 
+#define CS_GPIO GPIOB       
+#define CS_PIN GPIO_PIN_1   
 
-
-#define NOOP 0          // No operation
-#define DIGIT0 1        // zápis hodnoty na 1. cifru
-#define DIGIT1 2        // zápis hodnoty na 1. cifru
-#define DIGIT2 3        // zápis hodnoty na 1. cifru
-#define DIGIT3 4        // zápis hodnoty na 1. cifru
-#define DIGIT4 5        // zápis hodnoty na 1. cifru
-#define DIGIT5 6        // zápis hodnoty na 1. cifru
-#define DIGIT6 7        // zápis hodnoty na 1. cifru
-#define DIGIT7 8        // zápis hodnoty na 1. cifru
-#define DECODE_MODE 9   // Aktivace/Deaktivace znakové sady (my volíme vždy hodnotu DECODE_ALL)
-#define INTENSITY 10    // Nastavení jasu - argument je číslo 0 až 15 (větší číslo větší jas)
-#define SCAN_LIMIT 11   // Volba počtu cifer (velikosti displeje) - argument je číslo 0 až 7 (my dáváme vždy 7)
-#define SHUTDOWN 12     // Aktivace/Deaktivace displeje (ON / OFF)
-#define DISPLAY_TEST 15 // Aktivace/Deaktivace "testu" (rozsvítí všechny segmenty)
-
-// makra argumentů
-// argumenty pro SHUTDOWN
-#define DISPLAY_ON 1  // zapne displej
-#define DISPLAY_OFF 0 // vypne displej
-// argumenty pro DISPLAY_TEST
-#define DISPLAY_TEST_ON 1  // zapne test displeje
-#define DISPLAY_TEST_OFF 0 // vypne test displeje
-// argumenty pro DECODE_MOD
-#define DECODE_ALL 0b11111111 // (lepší zápis 0xff) zapíná znakovou sadu pro všechny cifry
-#define DECODE_NONE 0         // vypíná znakovou sadu pro všechny cifry
-
-
-#define CLK_GPIO GPIOB     // port na kterém je CLK vstup budiče
-#define CLK_PIN GPIO_PIN_0  // pin na kterém je CLK vstup budiče
-#define DATA_GPIO GPIOB     // port na kterém je DIN vstup budiče
-#define DATA_PIN GPIO_PIN_2 // pin na kterém je DIN vstup budiče
-#define CS_GPIO GPIOB       // port na kterém je LOAD/CS vstup budiče
-#define CS_PIN GPIO_PIN_1   // pin na kterém je LOAD/CS vstup budiče
-
+/* Makra na práci s Piny */
 #define CLK_HIGH GPIO_WriteHigh(CLK_GPIO, CLK_PIN)
 #define CLK_LOW GPIO_WriteLow(CLK_GPIO, CLK_PIN)
 #define DATA_HIGH GPIO_WriteHigh(DATA_GPIO, DATA_PIN)
@@ -46,60 +20,63 @@
 #define CS_HIGH GPIO_WriteHigh(CS_GPIO, CS_PIN)
 #define CS_LOW GPIO_WriteLow(CS_GPIO, CS_PIN)
 
+/* Nějaká super funkce co posílá data */
 void max7219_send(uint8_t address, uint8_t data)
 {
-    uint8_t mask; // pomocná proměnná, která bude sloužit k procházení dat bit po bitu
-    CS_LOW;       // nastavíme linku LOAD/CS do úrovně Low (abychom po zapsání všech 16ti bytů mohli vygenerovat na CS vzestupnou hranu)
+    uint8_t mask; 
+    CS_LOW;       
 
-    // nejprve odešleme prvních 8bitů zprávy (adresa/příkaz)
-    mask = 0b10000000; // lepší zápis je: maska = 1<<7
-    CLK_LOW;           // připravíme si na CLK vstup budiče úroveň Low
+    
+    mask = 0b10000000; 
+    CLK_LOW;          
     while (mask)
-    { // dokud jsme neposlali všech 8 bitů
+    { 
         if (mask & address)
-        {              // pokud má právě vysílaný bit hodnotu 1
-            DATA_HIGH; // nastavíme budiči vstup DIN do úrovně High
+        {              
+            DATA_HIGH;
         }
         else
-        {             // jinak má právě vysílaný bit hodnotu 0 a...
-            DATA_LOW; // ... nastavíme budiči vstup DIN do úrovně Low
+        {             
+            DATA_LOW; 
         }
-        CLK_HIGH;         // přejdeme na CLK z úrovně Low do úrovně High, a budič si zapíše hodnotu bitu, kterou jsme nastavili na DIN
-        mask = mask >> 1; // rotujeme masku abychom v příštím kroku vysílali nižší bit
-        CLK_LOW;          // vrátíme CLK zpět do Low abychom mohli celý proces vysílání bitu opakovat
+        CLK_HIGH;         
+        mask = mask >> 1; 
+        CLK_LOW;          
     }
 
-    // poté pošleme dolních 8 bitů zprávy (data/argument)
+   
     mask = 0b10000000;
     while (mask)
-    { // dokud jsme neposlali všech 8 bitů
+    {
         if (mask & data)
-        {              // pokud má právě vysílaný bit hodnotu 1
-            DATA_HIGH; // nastavíme budiči vstup DIN do úrovně High
+        {              
+            DATA_HIGH; 
         }
         else
-        {             // jinak má právě vysílaný bit hodnotu 0 a...
-            DATA_LOW; // ... nastavíme budiči vstup DIN do úrovně Low
+        {             
+            DATA_LOW;
         }
-        CLK_HIGH;         // přejdeme na CLK z úrovně Low do úrovně High, a v budič si zapíše hodnotu bitu, kterou jsme nastavili na DIN
-        mask = mask >> 1; // rotujeme masku abychom v příštím kroku vysílali nižší bit
-        CLK_LOW;          // vrátíme CLK zpět do Low abychom mohli celý proces vysílání bitu opakovat
+        CLK_HIGH;         
+        mask = mask >> 1;
+        CLK_LOW;          
     }
 
-    CS_HIGH; // nastavíme LOAD/CS z úrovně Low do úrovně High a vygenerujeme tím vzestupnou hranu (pokyn pro MAX7219 aby zpracoval náš příkaz)
+    CS_HIGH; 
 }
 
 void max7219_init(void)
-{
+{   /* Nastavení pinu do začáteční pozice */
     GPIO_Init(CS_GPIO, CS_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(CLK_GPIO, CLK_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(DATA_GPIO, DATA_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+
     // nastavíme základní parametry budiče
-    max7219_send(DECODE_MODE, DECODE_ALL);        // zapnout znakovou sadu na všech cifrách
-    max7219_send(SCAN_LIMIT, 3);                  // velikost displeje 8 cifer (počítáno od nuly, proto je argument číslo 7)
-    max7219_send(INTENSITY, 5);                   // volíme ze začátku nízký jas (vysoký jas může mít velkou spotřebu - až 0.25A !)
-    max7219_send(DISPLAY_TEST, DISPLAY_TEST_OFF); // Funkci "test" nechceme mít zapnutou
-    max7219_send(SHUTDOWN, DISPLAY_ON);           // zapneme displej
+    max7219_send(DECODE_MODE, DECODE_ALL);        // Nějakej dekodér
+    max7219_send(SCAN_LIMIT, 3);                  // Kolik cifer zapneme
+    max7219_send(INTENSITY, 5);                   // Jas
+    max7219_send(DISPLAY_TEST, DISPLAY_TEST_OFF); // test displeje
+    max7219_send(SHUTDOWN, SHUTDOWN_ON);           // On/OFF
+
 }
 
 void setup(void)
@@ -126,12 +103,13 @@ int main(void)
     max7219_send(DIGIT6, 0);
     max7219_send(DIGIT7, 0);
 
-    uint32_t time = 0;
-    uint32_t number = 0;
+    uint8_t time = 0;
+    uint8_t number = 0;
+    uint8_t desitky = 0;
+    uint8_t stovky = 0;
+    uint8_t tisice = 0;
 
-    uint32_t desitky = 0;
-    uint32_t stovky = 0;
-    uint32_t tisice = 0;
+    /* Funkce pro zobrazení čísel */
     while (1)
     {
         if ((milis() - time) > 200)
